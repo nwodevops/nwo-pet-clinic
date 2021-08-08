9d1e0cffb3e855a22193c0e448aff0db5dd6a841
nwo <nwoswo@gmail.com>
2021-08-07 19:20:30 -0500

    Initial Project Add

---
+++ src/main/java/com/nwo/nwopetclinic/NwoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.nwo.nwopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwoPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NwoPetClinicApplication.class, args);
+    }
+
+}
