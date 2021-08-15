267727b61d2bb6cef27522d3e5135a6b411012cd
nwodevops <nwodevops@gmail.com>
2021-08-15 08:39:58 -0500

    adding owners page and conroller . Closes #13

---
+++ pet-clinic-web/src/main/java/com/nwo/nwopetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.nwo.nwopetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+  @RequestMapping({"", "/index", "/index.html"})
+  public String listOwner(){
+    return "owners/index";
+  }
+  
+}
