6974c8cc4dd679770307c5ea4c913a902be2e7ee
Eder m50 Oswaldo <M50SW@hotmail.com>
2022-06-02 00:44:43 -0500

    Refactored map Imp ID closes #23

--- pet-clinic-data/src/main/java/com/nwo/nwopetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/nwo/nwopetclinic/services/map/PetServiceMap.java
@@ -34,7 +34,7 @@ public class PetServiceMap
 
   @Override
   public Pet save(Pet object) {
-    return super.save(object.getId(), object);
+    return super.save(object);
   }
 
 }
--- pet-clinic-data/src/main/java/com/nwo/nwopetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/nwo/nwopetclinic/services/map/VetServiceMap.java
@@ -22,7 +22,7 @@ public class VetServiceMap
   
     @Override
     public Vet save(Vet object) {
-      return super.save(object.getId(), object);
+      return super.save(object);
     }
   
     @Override
