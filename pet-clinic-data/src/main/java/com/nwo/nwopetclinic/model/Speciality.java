d122a4c03b3e8023de27cd904bd3a3247eac7cf8
Eder m50 Oswaldo <M50SW@hotmail.com>
2022-06-05 17:39:28 -0500

    closes #26

---
+++ pet-clinic-data/src/main/java/com/nwo/nwopetclinic/model/Speciality.java
@@ -0,0 +1,8 @@
+package com.nwo.nwopetclinic.model;
+
+import lombok.Data;
+
+@Data
+public class Speciality extends BaseEntity {
+    private String description;
+}
--- pet-clinic-data/src/main/java/com/nwo/nwopetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/nwo/nwopetclinic/model/Vet.java
@@ -1,5 +1,12 @@
 package com.nwo.nwopetclinic.model;
+
+import java.util.Set;
+
+import lombok.Data;
+
+@Data
 public class Vet extends Person {
 
-
+    private Set<Speciality> specialities;
+    
 }
