de96efaf4c5a54df920acac825a6e751cd1365a0
Eder m50 Oswaldo <M50SW@hotmail.com>
2022-06-05 17:43:04 -0500

    closes #27

--- pet-clinic-data/src/main/java/com/nwo/nwopetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/nwo/nwopetclinic/model/Owner.java
@@ -5,6 +5,10 @@ import java.util.Set;
 import lombok.Data;
 
 
-@public class Owner extends  Person{
+@Data
+public class Owner extends  Person{
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 }
