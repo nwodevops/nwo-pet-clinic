79611bdc845f637fc5a0c8d5e1a4377ab5c4dd8f
Eder m50 Oswaldo <M50SW@hotmail.com>
2022-06-06 12:32:58 -0500

    Closes #28

---
+++ pet-clinic-data/src/main/java/com/nwo/nwopetclinic/services/map/PetTypeMap.java
@@ -0,0 +1,38 @@
+package com.nwo.nwopetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.nwo.nwopetclinic.model.PetType;
+import com.nwo.nwopetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+    
+}
