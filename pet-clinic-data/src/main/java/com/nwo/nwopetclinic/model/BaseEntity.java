8b573e7414c0696364ed1e823357573c60cb48a7
Eder m50 Oswaldo <M50SW@hotmail.com>
2022-06-07 12:28:37 -0500

    Update base entity with JPA annotation. Closes #34

--- pet-clinic-data/src/main/java/com/nwo/nwopetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/nwo/nwopetclinic/model/BaseEntity.java
@@ -2,12 +2,20 @@ package com.nwo.nwopetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@MappedSuperclass
 @Setter @Getter
 public class BaseEntity implements Serializable{
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   
   
