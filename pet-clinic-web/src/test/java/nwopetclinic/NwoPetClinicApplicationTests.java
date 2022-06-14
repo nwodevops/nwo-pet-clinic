68c2c3f8561ba988a6e75cbf46468f4cb1607225
Eder m50 Oswaldo <M50SW@hotmail.com>
2022-06-14 16:29:42 -0500

    fix load

--- pet-clinic-web/src/test/java/nwopetclinic/NwoPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/nwopetclinic/NwoPetClinicApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(classes = NwoPetClinicApplicationTests.class)
 class NwoPetClinicApplicationTests {
 
     @Test
