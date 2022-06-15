56fd63d1274cbc730d5cc8361b617e24b6b9dbe6
Eder m50 Oswaldo <M50SW@hotmail.com>
2022-06-15 11:39:55 -0500

    close #58

--- pet-clinic-data/src/main/java/com/nwo/nwopetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/nwo/nwopetclinic/model/PetType.java
@@ -5,11 +5,23 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
-@Data
 @Table(name = "types")
 public class PetType extends BaseEntity{
 
     @Column(name = "name")
     private String name;
+
+    // @Override
+    // public String toString(){
+    //     return name;
+    // }
 }
--- pet-clinic-web/src/main/java/com/nwo/nwopetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/nwo/nwopetclinic/controllers/OwnerController.java
@@ -1,10 +1,14 @@
 package com.nwo.nwopetclinic.controllers;
 
+import com.nwo.nwopetclinic.model.Owner;
 import com.nwo.nwopetclinic.services.OwnerService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -24,5 +28,18 @@ public class OwnerController {
     model.addAttribute("owners", ownerService.findAll());
     return "owners/index";
   }
+
+  @RequestMapping("/find")
+    public String findOwners(Model model){
+        model.addAttribute("owner", Owner.builder().build());
+        return "owners/findOwners";
+    }
+
+  @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
   
 }
